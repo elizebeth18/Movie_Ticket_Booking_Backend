4926014e833ac7dd191909a58f5015f3728a8761
Jilu Elizebeth <elizebeth18.jilu@gmail.com>
2026-09-10 18:46:30 +0530

    GET method to fetch all movies

--- src/routers/movies.js
+++ src/routers/movies.js
@@ -9,11 +9,20 @@ router.post('/movies', async (req, res, next) => {
         const savedMovie = await movie.save();
         console.log(savedMovie)
         res.status(201).send(savedMovie)
-    } catch(error){
+    } catch (error) {
         console.log(error)
         res.status(400).send(error)
     }
 });
 
+router.get('/movies', async (req, res, next) => {
+    try {
+        const movies = await Movie.find(); //fetches all movies from the db
+        console.log('movies in GET method', movies)
+        res.status(200).send(movies)
+    } catch (error) {
+        res.status(500).send()
+    }
+})
 
 module.exports = router;
